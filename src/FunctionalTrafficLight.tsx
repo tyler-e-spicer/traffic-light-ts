974bdb7c931c50392ab5596ef5fa361f6f67a182
tyler-e-spicer <tyler.e.spicer@gmail.com>
2024-06-12 16:14:49 -0500

    functional component review complete

--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
+import { TrafficLightState } from "../types";
+
 export const FunctionalTrafficLight = () => {
+  const [trafficLight, setTrafficLight] = useState<TrafficLightState>("red");
+
+  const handleLight = () => {
+    if (trafficLight === "red") {
+      setTrafficLight("green");
+    } else if (trafficLight === "green") {
+      setTrafficLight("yellow");
+    } else if (trafficLight === "yellow") {
+      setTrafficLight("red");
+    }
+  };
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div
+          className={`circle ${trafficLight === "red" ? "red" : "black"}`}
+        ></div>
+        <div
+          className={`circle ${trafficLight === "yellow" ? "yellow" : "black"}`}
+        ></div>
+        <div
+          className={`circle ${trafficLight === "green" ? "green" : "black"}`}
+        ></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={handleLight}>
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
---
+++ types.ts
@@ -0,0 +1,1 @@
+export type TrafficLightState = "red" | "yellow" | "green";
